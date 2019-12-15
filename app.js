ece00561a8b22ea20871aa0d77cd5aca246872bf
andrey.ovcharenko@branderstudio.com <andrey.ovcharenko@branderstudio.com>
2019-12-15 23:45:48 +0200

    add girigmnore

--- app.js
+++ app.js
@@ -7,22 +7,59 @@ const app       = express()
 
 expressWs(app)
 
-const fs = require('fs')
-let i = 0;
-app.ws('/ws', (ws, req) => {
-    ws.on('message', msg => {
-        console.log(msg)
-        fs.writeFile(`./mp4/${i++}.mpeg`, msg, function(err) {console.log(err)})
-    })
 
-    ws.on('close', () => {
-        console.log('WebSocket was closed')
-    })
-})
+app.use(express.static('static'))
+
+const child_process = require('child_process');
+
+// -profile:v baseline -level 3.0 -s 640x360 -start_number 0 -hls_time 10 -hls_list_size 0 -f hls index.m3u8
+
+app.ws('/ws', (wss, req) => {
+
+    const rtmpUrl = './test.avi';
+    console.log('Target RTMP URL:', rtmpUrl);
+
+    // Launch FFmpeg to handle all appropriate transcoding, muxing, and RTMP
+    const ffmpeg = child_process.spawn('ffmpeg', [
+      '-i', '-',
+      '-hls_list_size', '6',
+      '-hls_time', '10',
+      '-f', 'hls',
+
+      './static/index.m3u8'
+    ]);
+
+    // If FFmpeg stops for any reason, close the WebSocket connection.
+    ffmpeg.on('close', (code, signal) => {
+      console.log('FFmpeg child process closed, code ' + code + ', signal ' + signal);
+      wss.terminate();
+    });
+
+    // Handle STDIN pipe errors by logging to the console.
+    // These errors most commonly occur when FFmpeg closes and there is still
+    // data to write.  If left unhandled, the server will crash.
+    ffmpeg.stdin.on('error', (e) => {
+      console.log('FFmpeg STDIN Error', e);
+    });
+
+    // FFmpeg outputs all of its messages to STDERR.  Let's log them to the console.
+    ffmpeg.stderr.on('data', (data) => {
+      console.log('FFmpeg STDERR:', data.toString());
+    });
+
+    // When data comes in from the WebSocket, write it to FFmpeg's STDIN.
+    wss.on('message', (msg) => {
+      console.log('DATA', msg);
+      ffmpeg.stdin.write(msg);
+    });
+
+    // If the client disconnects, stop FFmpeg.
+    wss.on('close', (e) => {
+      ffmpeg.kill('SIGINT');
+    });
 
 
+});
 
-
-app.use(express.static('static'));
 
 app.listen(3000);
